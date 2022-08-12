71c2edb916612cc42cc4e597d9e5c6b330b95d0d
Kevin Hernandez <hernandez.kevin@galileo.edu>
2022-08-11 18:31:46 -0600

    Added small tutorial on how to use the file system to make this program work

--- esp_files/pwm.py
+++ esp_files/pwm.py
@@ -2,6 +2,14 @@
 from machine import Pin, PWM
 
 # Testing out importing Libraries
+'''
+    To make this work as intended, utilize Ampy to manipulate files over your ESP.
+    The steps to accomplish that are as follow:
+    1. Open command prompt and make sure you are in the same directory as the 'dummy_library.py'
+    2. Write the file 'dummy_library.py' onto de ESP using the command:
+    ampy -pCOMzz put dummy_library.py
+    3. Run this file
+'''
 from dummy_library import DummyClass
 
 # Setting up parameters
